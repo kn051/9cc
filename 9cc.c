f02deabdcb051480449bfa9499d2ee14ffda32b8
西川恭平 <nishikawakyohei@nishikawakyouheinoMacBook-Air.local>
2022-11-05 07:09:30 +0900

    パーサの実装

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+//
+// トークナイザー
+//
+
 // トークンの種類
 typedef enum {
   TK_RESERVED, // 記号
@@ -39,11 +43,13 @@ void error(char *fmt, ...) {
 }
 
 // エラー箇所を報告する
+// loc は入力全体を表す文字列の途中を指しているポインタ
+// fmt は入力の先頭を指しているポインタ
 void error_at(char *loc, char *fmt, ...) {
   va_list ap; 
   va_start(ap, fmt); // 可変長引数を1個の変数にまとめる
 
-  int pos = loc - user_input;
+  int pos = loc - user_input; // 文字列の途中と先頭を指すポインタの差
   fprintf(stderr, "%s\n", user_input);
   fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
   fprintf(stderr, "^ ");
@@ -106,7 +112,7 @@ Token *tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (strchr("+-*/()", *p)) {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -124,39 +130,164 @@ Token *tokenize() {
   return head.next;
 }
 
+//
+// パーサー
+//
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    error("引数の個数が正しくありません");
-    return 1;
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+// ノードの型
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+// 目的：Nodeを新しく作る
+// new_node : NodeKind -> Node
+Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+// 目的：右辺と左辺を受け取る2項演算子のNodeを作る
+// new_binary : NodeKind -> Node -> Node -> Node
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+// 目的：数値のノードを新しく作る
+// new_num : int -> Node
+Node *new_num(int val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+// 再帰的に使えるように先に宣言しておく
+Node *expr();
+Node *mul();
+Node *primary();
+
+// 目的：＋とーの演算子をパースする
+// expr : Node
+// expr = mul ("+" mul | "-" mul)*
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_binary(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_binary(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+// 目的：*と/の演算子をパースする
+// mul : Node
+// mul = primary ("*" primary | "/" primary)*
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_binary(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_binary(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+// 目的：(expr)とnumをパースする
+// primary : Node
+// primary = "(" expr ")" | num
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
   }
 
-  user_input = argv[1];
+  return new_num(expect_number());
+}
 
-  // トークナイズする
+
+//
+// Code generator
+//
+
+// 目的：Node を受け取り、スタックマシンの要領でアセンブリコードを吐き出す
+// gen : Node -> アセンブリコードの吐き出し
+void gen (Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
+
+
+
+int main(int argc, char **argv) {
+  if (argc != 2)
+    error("%s: 引数の個数が正しくありません", argv[0]);
+
+  // トークナイズしてパースする
+  user_input = argv[1];
   token = tokenize();
+  Node *node = expr();
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
-  printf(".globl main\n");
+  printf(".global main\n");
   printf("main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
-  // 最初の mov 命令を出力
-  printf("  mov rax, %ld\n", expect_number());
+  // 抽象構文木を降りながらコード生成
+  gen(node);
 
-  // '+ <数>' あるいは '- <数>' というトークンの並びを消費しつつ
-  // アセンブリを出力
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %ld\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %ld\n", expect_number());
-  }
-
+  // スタックトップに式全体の値が残っているはずなので、
+  // それをRAXにロードして関数からの返り値とする
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
